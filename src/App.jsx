35b7dc6e48deb10c09cd4716c2e3918777b30acf
Tiago Soft Dev <tiago.soft.dev@gmail.com>
2024-08-06 16:52:27 -0300

    Arquivos iniciais do projeto

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Card from './components/Card/Card'
+import Header from './components/Header/Header'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Card />
+      <Card />
+
+    </>
+  )
+}
+
+export default App
