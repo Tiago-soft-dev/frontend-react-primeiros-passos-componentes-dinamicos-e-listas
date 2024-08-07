3600ebf82f3ce4adb38110d735fa3a8110a6261d
Tiago Soft Dev <tiago.soft.dev@gmail.com>
2024-08-06 21:53:02 -0300

    Elemento da lista com estilização adicionado.

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,11 @@ function App() {
   return (
     <>
       {/* <Header /> */}
-      <Card nome="Java" imagem="https://salvatore.academy/devmon/1_java.png"/>
-      <Card nome="Kotlin" imagem="https://salvatore.academy/devmon/2_kotlin.png"/>
-      <Card nome="Android" imagem="https://salvatore.academy/devmon/3_android.png"/>
+      <div className='cards'>
+        <Card nome="Java" imagem="https://salvatore.academy/devmon/1_java.png" />
+        <Card nome="Kotlin" imagem="https://salvatore.academy/devmon/2_kotlin.png" />
+        <Card nome="Android" imagem="https://salvatore.academy/devmon/3_android.png" />
+      </div>
 
     </>
   )
