e00e48924903e6e97d6535a2ece973b3dc5dc7bb
Tiago Soft Dev <tiago.soft.dev@gmail.com>
2024-08-07 09:31:52 -0300

    Transformação do Objeto Devmon em Array para uso com o Map e retorno do componente `Card`.

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,48 @@ import Card from './components/Card/Card'
 import Header from './components/Header/Header'
 
 function App() {
+
+  const devmonss = {
+    java: {
+      nome: 'Java',
+      imagem: 'https://salvatore.academy/devmon/1_java.png'
+    },
+    kotlin: {
+      nome: 'Kotlin',
+      imagem: 'https://salvatore.academy/devmon/2_kotlin.png'
+    },
+    android: {
+      nome: 'Android',
+      imagem: 'https://salvatore.academy/devmon/3_android.png'
+    }
+  }
+
+  const devmonArray = Object.entries(devmonss).map(([key, value]) => {
+    return {
+      chave: key,
+      nome: value.nome,
+      imagem: value.imagem
+    }
+  })
+
   return (
     <>
-      {/* <Header /> */}
+      Lista de Cards com Map:
+      <br />
+      <br />
+      <Header />
+      <div className='cards'>
+        {devmonArray.map((value) => {
+          return <Card nome={value.nome} imagem={value.imagem} key={value.chave} />
+          })}
+      </div>
+
+      <br />
+      <br />
+      Lista de Cards 2:
+      <br />
+      <br />
+      <Header />
       <div className='cards'>
         <Card nome="Java" imagem="https://salvatore.academy/devmon/1_java.png" />
         <Card nome="Kotlin" imagem="https://salvatore.academy/devmon/2_kotlin.png" />
