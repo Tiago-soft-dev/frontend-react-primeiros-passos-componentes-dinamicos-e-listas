1e9331c56f86f7f8d51e8bfa04844787322cbcba
Tiago Soft Dev <tiago.soft.dev@gmail.com>
2024-08-07 09:32:46 -0300

    Atualização da descrição sobre os Devmons.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
         <>
             <div className='header'>
                 <h1>Eu sou um DevMon</h1>
-                <p>Os DevMons são criaturinhas resultantes da fusão de pokémons com tecnologias de software a partir da mente divagante do grande Paulo Salvatore - CEO da Salvatore Academy.</p>
+                <p>Os DevMons são criaturinhas resultantes da fusão de pokemons com tecnologias de software criadas a partir da mente disruptiva e inovadora do grande Paulo Salvatore - CEO da Salvatore Academy.</p>
             </div>
         </>
     )
